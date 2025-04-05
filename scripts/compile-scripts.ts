2ded2fa989fa0a1b2c5d429d3b41c15c50b884cc
Dávid Szakállas <david.szakallas@gmail.com>
2025-04-05 14:26:20 +0200

    modernize bundling

--- scripts/compile-scripts.ts
+++ scripts/compile-scripts.ts
@@ -61,8 +61,13 @@ const bundle = await rollup({
       exclude: ['node_modules/@babel/runtime/**'],
       extensions: ['.ts', '.js'],
       include: resolve('packages', '**', '*.ts'),
-      configFile: resolve('babel.config.js'),
       babelHelpers: 'runtime',
+      targets: { node: '10.0' },
+      presets: ['@babel/preset-typescript', ['@babel/preset-env', { modules: false }]],
+      plugins: [
+        ['@babel/plugin-transform-arrow-functions'], // work around Qt bug in arrow functions https://bugreports.qt.io/browse/QTBUG-95677
+        ['@babel/plugin-transform-runtime', { corejs: 3, regenerator: false, absoluteRuntime: true }],
+      ],
     }),
   ],
 })
